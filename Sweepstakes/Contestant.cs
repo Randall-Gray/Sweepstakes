bb929bdb77c25b832b50ccf749d59fecaeb0f903
Randall Gray <randall.gray7260@gmail.com>
2020-05-03 15:19:06 -0500

    Initial code

---
+++ Sweepstakes/Contestant.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sweepstakes
+{
+    public class Contestant
+    {
+        // Member variables
+        public string firstName;
+        public string lastName;
+        public string emailAddress;
+        public int registrationNumber;
+
+        // constructor
+
+        // Member methods
+    }
+}
